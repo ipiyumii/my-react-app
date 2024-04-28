2778fd4477d6db0ad131942198668c35e359b9c8
ipiyumii <sendtopiyumi@gmail.com>
2024-04-28 13:22:17 +0530

    module-css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Food from './Food.jsx';
 import Card from './Card.jsx';
+import Button from './Button/Button.jsx';
 
 function App() {
 
@@ -14,7 +15,7 @@ function App() {
       <Card/>
       <Card/>
       <Card/>
-
+      <Button/>
     </>
     
   );
