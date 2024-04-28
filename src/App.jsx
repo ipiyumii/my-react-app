f32f8478a408a8927ba3fc06800ff4d1a02db22e
ipiyumii <sendtopiyumi@gmail.com>
2024-04-28 15:48:13 +0530

    create-props

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './Footer.jsx'
 import Food from './Food.jsx';
 import Card from './Card.jsx';
 import Button from './Button/Button.jsx';
+import Student from './Student.jsx';
 
 function App() {
 
@@ -16,6 +17,11 @@ function App() {
       <Card/>
       <Card/>
       <Button/>
+      <Student name="piyumi" age={30} isStudent={true}/>
+      <Student name="imalka" age={22} isStudent={false}/>
+      <Student name="sinethu" age={11} isStudent={true}/>
+      <Student name="piumx" age={50} isStudent={false}/>
+
     </>
     
   );
